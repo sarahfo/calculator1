c1c33533397fc04d8b533d5a1b017ea0ac9a2909
Hackbright Student <info@hackbrightacademy.com>
2015-04-09 12:58:19 -0700

    changed the addition function

---
+++ arithmetic.py
@@ -0,0 +1,25 @@
+#Prefix Calculator Exercise
+#Addition function
+def add(num1, num2):    
+    return num1 + num2    
+
+# def subtract(num1, num2):
+#     pass
+
+# def multiply(num1, num2):
+#     pass
+
+# def divide(num1, num2):
+#     pass
+
+# def square(num1):
+#     pass
+
+# def cube(num1):
+#     pass
+
+# def power(num1, num2):
+#     pass
+
+# def mod(num1, num2):
+#     pass
